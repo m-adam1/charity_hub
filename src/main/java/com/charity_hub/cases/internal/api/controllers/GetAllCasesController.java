0b38d787f98d481d527ebc989c402bd39cef117b
Muhammad Adam <>
2025-05-12 01:52:28 +0300

    build: Add datadog dependencies

--- src/main/java/com/charity_hub/cases/internal/api/controllers/GetAllCasesController.java
+++ src/main/java/com/charity_hub/cases/internal/api/controllers/GetAllCasesController.java
@@ -4,6 +4,7 @@ import com.charity_hub.cases.internal.api.dtos.GetCasesRequest;
 import com.charity_hub.cases.internal.application.queries.GetAllCases.GetAllCasesQuery;
 import com.charity_hub.cases.internal.infrastructure.queryhandlers.GetAllCasesHandler;
 import com.charity_hub.shared.api.DeferredResults;
+import com.charity_hub.shared.domain.ILogger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -14,9 +15,11 @@ import org.springframework.web.context.request.async.DeferredResult;
 public class GetAllCasesController {
 
     private final GetAllCasesHandler getAllCasesHandler;
+    private final ILogger logger;
 
-    public GetAllCasesController(GetAllCasesHandler getAllCasesHandler) {
+    public GetAllCasesController(GetAllCasesHandler getAllCasesHandler, ILogger logger) {
         this.getAllCasesHandler = getAllCasesHandler;
+        this.logger = logger;
     }
 
     @GetMapping("/v1/cases")
@@ -28,6 +31,9 @@ public class GetAllCasesController {
                 Math.max(request.offset(), 0),
                 Math.min(Math.max(request.limit(), 1), 100)
         );
+
+        logger.warn("adam: Getting all cases");
+
         return DeferredResults.from(getAllCasesHandler.handle(query).thenApply(ResponseEntity::ok));
     }
 }
